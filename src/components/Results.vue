8987b46a0ae005c6675b142951fdc13432674a0a
Soumodip Das <soumodipd567@gmail.com>
2023-08-02 15:02:17 +0530

    done

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+   props: ['score'],
+   data(){
+    return{
+      rank: null
+    }
+   },
+   mounted(){
+        if(this.score < 350) {
+            this.rank = 'Mega Knight'
+        } else if (this.score < 450) {
+            this.rank = 'Knight'
+        } else {
+            this.rank = 'Skeleton'
+        }
+   }
+}
+</script>
+
+<style>
+   .rank {
+    font-size: 1.4em;
+    color: #007BFF;
+    font-weight: bold;
+   }
+</style>
